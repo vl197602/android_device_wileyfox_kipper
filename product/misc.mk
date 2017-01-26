6ace971fabedc9878d049c7fae2e878ae6ce5a6c
vl197602 <radarga@gmail.com>
2017-01-26 11:23:59 +0300

    Disable some features

--- product/misc.mk
+++ product/misc.mk
@@ -2,18 +2,8 @@
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.opa.eligible_device=true
 
-# Better signal
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.cust.tel.eons=1 \
-ro.config.hw_fast_dormancy=1
-
 # Better call voice quality
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.ril.enable.amr.wideband=1
 
-# Saves power
-PRODUCT_PROPERTY_OVERRIDES += \
-pm.sleep_mode=1 \
-ro.ril.disable.power.collapse=0 \
-ro.ril.power_collapse=1
 
